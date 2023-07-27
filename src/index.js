67bd7ca90f1565d5639690ced263d3b311e34618
Brito <alexandre.cunh4@gmail.com>
2023-07-27 20:02:07 -0300

    create routes for categories mainly post

--- src/index.js
+++ src/index.js
@@ -1,22 +1,19 @@
 const express = require("express");
 const db = require("./db");
+const routerCategories = require("./routes/categories");
 
 const app = express();
+
+app.use(express.json());
+
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/categories", (req, res) => {
-  db.query("SELECT * FROM categories", (error, response) => {
-    if (error) {
-      return res.status(500).json(error);
-    }
+app.use("/categories", routerCategories);
 
-    return res.status(200).json(response.rows);
-  });
-});
 app.listen(port, () => {
   db.connect()
     .then(() => {
