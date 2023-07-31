49259393631bf3c90cf04a0cba40d8038f53c2ea
Brito <alexandre.cunh4@gmail.com>
2023-07-30 22:39:49 -0300

    put method for categories

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -12,13 +12,16 @@ const findOne = (id) => {
 
 router.get("/", (req, res) => {
   try {
-    db.query("SELECT * FROM categories", (error, response) => {
-      if (error) {
-        return res.status(500).json(error);
-      }
+    db.query(
+      "SELECT * FROM categories ORDER BY name ASC ",
+      (error, response) => {
+        if (error) {
+          return res.status(500).json(error);
+        }
 
-      return res.status(200).json(response.rows);
-    });
+        return res.status(200).json(response.rows);
+      }
+    );
   } catch (error) {
     res.status(500).json(error);
   }
@@ -31,7 +34,7 @@ router.post("/", (req, res) => {
     if (name.length < 3) {
       return res
         .status(400)
-        .json({ error: "Name should have more than 3 characters " });
+        .json({ error: "Name should have more than 3 characters." });
     }
 
     const text = "INSERT INTO categories(name) VALUES($1) RETURNING *";
@@ -60,18 +63,53 @@ router.delete("/:id", async (req, res) => {
     const query = findOne(id);
     const category = await db.query(query);
     if (!category.rows[0]) {
-      return res.status(404).json({ error: "Category not found" });
+      return res.status(404).json({ error: "Category not found." });
     }
 
     const text = "DELETE FROM categories WHERE id = $1 RETURNING *";
     const values = [Number(id)];
     const deleteResponse = await db.query(text, values);
 
-    if (!category.rows[0]) {
-      return res.status(400).json({ error: "Category not deleted" });
+    if (!deleteResponse.rows[0]) {
+      return res.status(400).json({ error: "Category not deleted." });
     }
 
     return res.status(200).json(deleteResponse.rows[0]);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name } = req.body;
+
+    if (!id) {
+      return res.status(400).json({ error: "Param id is mandatory." });
+    }
+
+    if (name.length < 3) {
+      return res
+        .status(400)
+        .json({ error: "Name should have more than 3 characters." });
+    }
+
+    const query = findOne(id);
+    const category = await db.query(query);
+    if (!category.rows[0]) {
+      return res.status(404).json({ error: "Category not found." });
+    }
+
+    const text = "UPDATE categories SET name = $1 WHERE id = $2 RETURNING *";
+    const values = [name, Number(id)];
+    const updateResponse = await db.query(text, values);
+
+    if (!updateResponse.rows[0]) {
+      return res.status(400).json({ error: "Category not updated." });
+    }
+
+    return res.status(200).json(updateResponse.rows[0]);
   } catch (error) {
     res.status(500).json(error);
   }
